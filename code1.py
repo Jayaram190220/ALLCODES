a87dfcc2c18a86a4e238fe6960cbf12c0b25f1eb
Jayaram <jayaramvennapu.2000@gmail.com>
2025-03-12 16:58:15 +0000

    code 1 python

---
+++ code1.py
@@ -0,0 +1,12 @@
+        
+        
+        
+        
+str1=str(input("enter a string:"))
+
+b= int(input("enter a number:"))
+
+if b<len(str1):
+    print(str1[b:-1])
+else:
+    print("Adi panicheyadu")
