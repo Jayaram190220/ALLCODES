5694db52bddfd47a8273f76d3d5b51f864e7651f
Jayaram <jayaramvennapu.2000@gmail.com>
2025-03-22 09:07:52 +0000

    problem-1

---
+++ prob1.py
@@ -0,0 +1,8 @@
+#Question1
+int_num1= int(input("number 1: "))
+int_num2= int(input("number 2: "))
+
+if (int_num1*int_num2)<=1000:
+    print((int_num1*int_num2))
+else:
+    print((int_num1+int_num2))
